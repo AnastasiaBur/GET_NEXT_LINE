1b0f2318870b2b75d32b3955c2ab66814b0b165b
Jayne bracken Sanford <jsanford@e3r8p1.21-school.ru>
2018-12-27 15:59:46 +0300

    1

---
+++ 21/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanford <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/27 15:51:52 by jsanford          #+#    #+#             */
+/*   Updated: 2018/12/27 15:57:58 by jsanford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+# define #define BUFF_SIZE 10
+
+int		get_next_line(const int fd, char **line);
+
+#endif
